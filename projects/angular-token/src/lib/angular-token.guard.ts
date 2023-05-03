cf0f424d0548fd5c808e68460cf19625d7e785d3
Ashok <kingkoirala1@gmail.com>
2023-05-03 20:09:10 +0545

    fix: token

---
+++ projects/angular-token/src/lib/angular-token.guard.ts
@@ -0,0 +1,54 @@
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AngularTokenService } from './angular-token.service';
+import {  of } from 'rxjs';
+import { catchError, map} from 'rxjs/operators'
+
+export const AuthGuard = () => {
+  const authService = inject(AngularTokenService);
+  const router = inject(Router);
+  const localStorage: Storage | any = {};
+  return authService.validateToken().pipe(
+    map((res) => {
+      if (authService.haveAuthParams()) {
+        return true;
+      }
+      if (authService.tokenOptions.signInStoredUrlStorageKey) {
+        localStorage.setItem(
+          authService.tokenOptions.signInStoredUrlStorageKey,
+          router.url
+        );
+      }
+      return router.parseUrl(
+        authService.tokenOptions.signInPath || 'auth/sign-in'
+      );
+    }),
+    catchError((error) => {
+      if (authService.tokenOptions.signInStoredUrlStorageKey) {
+        localStorage.setItem(
+          authService.tokenOptions.signInStoredUrlStorageKey,
+          router.url
+        );
+      }
+      return of(
+        router.parseUrl(authService.tokenOptions.signInPath || 'auth/sign-in')
+      );
+    })
+  );
+};
+
+export const IsLoginGuard = () => {
+  const authService = inject(AngularTokenService);
+  const router = inject(Router);
+
+  if (!authService.haveAuthParams()) {
+    return true;
+  }
+  if (authService.tokenOptions.signInStoredUrlStorageKey) {
+    return localStorage.getItem(
+      authService.tokenOptions.signInStoredUrlStorageKey
+    );
+  }
+  return router.parseUrl(authService.fetchSignInRedirect() || '');
+};
--- projects/angular-token/src/public_api.ts
+++ projects/angular-token/src/public_api.ts
@@ -1,59 +1,4 @@
 
-import {  inject } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { AngularTokenService } from './lib/angular-token.service';
-import {  of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
-
-
-export const AuthGuard = () => {
-  const authService = inject(AngularTokenService);
-  const router = inject(Router);
-  const localStorage: Storage | any = {};
-  return authService.validateToken().pipe(
-    map((res) => {
-      if (authService.haveAuthParams()) {
-        return true;
-      }
-      if (authService.tokenOptions.signInStoredUrlStorageKey) {
-        localStorage.setItem(
-          authService.tokenOptions.signInStoredUrlStorageKey,
-          router.url
-        );
-      }
-      return router.parseUrl(
-        authService.tokenOptions.signInPath || 'auth/sign-in'
-      );
-    }),
-    catchError((error) => {
-      if (authService.tokenOptions.signInStoredUrlStorageKey) {
-        localStorage.setItem(
-          authService.tokenOptions.signInStoredUrlStorageKey,
-          router.url
-        );
-      }
-      return of(
-        router.parseUrl(authService.tokenOptions.signInPath || 'auth/sign-in')
-      );
-    })
-  );
-};
-
-export const IsLoginGuard = () => {
-  const authService = inject(AngularTokenService);
-  const router = inject(Router);
-
-  if (!authService.haveAuthParams()) {
-    return true;
-  }
-  if (authService.tokenOptions.signInStoredUrlStorageKey) {
-    return localStorage.getItem(
-      authService.tokenOptions.signInStoredUrlStorageKey
-    );
-  }
-  return router.parseUrl(authService.fetchSignInRedirect() || '');
-};
 
 export {
   SignInData,
@@ -72,3 +17,5 @@ export { ANGULAR_TOKEN_OPTIONS } from './lib/angular-token.token';
 export { AngularTokenService } from './lib/angular-token.service';
 
 export { AngularTokenModule } from './lib/angular-token.module'; 
+
+export { AuthGuard, IsLoginGuard } from './lib/angular-token.guard'; 
